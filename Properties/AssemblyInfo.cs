a3aa7666e971c81726afec0a7f85ac6f8e0134e7
WYH2004 <32890512+WYH2004-MC@users.noreply.github.com>
2025-03-21 12:58:25 +0800

    chore: 更新项目配置和依赖

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,35 +1,23 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using MelonLoader;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mu3_Assist")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mu3_Assist")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle(Mu3_Assist.BuildInfo.Description)]
+[assembly: AssemblyDescription(Mu3_Assist.BuildInfo.Description)]
+[assembly: AssemblyCompany(Mu3_Assist.BuildInfo.Company)]
+[assembly: AssemblyProduct(Mu3_Assist.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Mu3_Assist.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Mu3_Assist.BuildInfo.Company)]
+[assembly: AssemblyVersion(Mu3_Assist.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Mu3_Assist.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(Mu3_Assist.Mu3Assist), Mu3_Assist.BuildInfo.Name, Mu3_Assist.BuildInfo.Version, Mu3_Assist.BuildInfo.Author, Mu3_Assist.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("AE986C08-44AE-4CAB-B059-3942B70AFB83")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonGame(null, null)]
